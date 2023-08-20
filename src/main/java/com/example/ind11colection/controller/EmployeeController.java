26cd7b29d943b7611cb711487eac671895bafbb1
Андрей <ustu1982@mail.ru>
2023-08-20 10:28:18 +0500

    bibliry

--- src/main/java/com/example/ind11colection/controller/EmployeeController.java
+++ src/main/java/com/example/ind11colection/controller/EmployeeController.java
@@ -1,30 +1,51 @@
 package com.example.ind11colection.controller;
+import com.example.ind11colection.exceptions.NotValidArgumentException;
 import com.example.ind11colection.medal.Employee;
 import com.example.ind11colection.servis.EmployeeServis;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/employee")
 
 public class EmployeeController {
 
     private final EmployeeServis servis;
-    public EmployeeController(EmployeeServis servis){
-        this.servis = servis;
+    private EmployeeController service;
+
+    public EmployeeController(EmployeeServis service){
+
+        this.servis = service;
     }
 
     @GetMapping("/add")
     public void add(@RequestParam String firstName, @RequestParam String lastName){
-    servis.addEmployee(firstName, lastName);
+        check(firstName, lastName);
+        servis.addEmployee(firstName, lastName);
     }
     @GetMapping("/get")
     public Employee get(@RequestParam String firstName, @RequestParam String lastName){
+        check(firstName, lastName);
 return servis.findEmployee(firstName, lastName);
     }
     @GetMapping("/remove")
     public boolean remove(@RequestParam String firstName, @RequestParam String lastName){
+        check(firstName, lastName);
     return servis.removeEmployee(firstName, lastName);
+    }
+    @GetMapping("/get")
+    public Collection<Employee> getAll(){
+        return service.getAll();
+    }
+    private void check(String... args){
+        for (String arg : args){
+            if (StringUtils.isAlpha(arg)){
+                throw new NotValidArgumentException();
+            }
+        }
     }
 }
