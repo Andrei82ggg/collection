b48b26d6549b05b797b01e2af3d29c6de2e1bb1e
Андрей <ustu1982@mail.ru>
2023-09-12 14:08:31 +0500

    sprin2

--- src/test/java/com/example/ind11colection/servis/DepartmentServiceTest.java
+++ src/test/java/com/example/ind11colection/servis/DepartmentServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.ind11colection.servis;
 
+import com.example.ind11colection.exceptions.EmployeeStorageIsFullException;
 import com.example.ind11colection.medal.Employee;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,10 +27,15 @@ class DepartmentServiceTest {
 @BeforeEach
     void setUp() {
     var employees = List.of(
-            new Employee("test1", "test_test1", 10, 1),
-            new Employee("test2", "test_test2", 20, 2),
-            new Employee("test3", "test_test3", 30, 3),
-            new Employee("test4", "test_test4", 50, 4)
+            new Employee("test", "test_test", 10, 1),
+            new Employee("test1", "test_test1", 20, 2),
+            new Employee("test2", "test_test2", 30, 1),
+            new Employee("test3", "test_test3", 40, 3),
+            new Employee("test33", "test_test33", 5, 3),
+            new Employee("test4", "test_test4", 50, 4),
+            new Employee("test5", "test_test5", 60, 1)
+
+
 
     );
     when(employeeService.getAll()).thenReturn(employees);
@@ -47,11 +54,39 @@ class DepartmentServiceTest {
     void testMinSalary(){
         Assertions.assertThat(departmentService.minSalary(1)).isEqualTo(30d);
     }
+
+    @Test
+    testWhenEmployeesIsEmpty(){
+    when(employeeService.getAll().thenReturn(Collections.emptyList()));
+    Assertions.assertThatThrownby(() -> departmentService.minSalary(1)).isIntanceof(EmployeeStorageIsFullException.class);
+        Assertions.assertThatThrownby(() -> departmentService.maxSalary(1)).isIntanceof(EmployeeStorageIsFullException.class);
+        Assertions.assertThat(departmentService.sum(1)).isEqualTo(0d);
+    }
     @Test
     void testAllByDept(){
         var employees = departmentService.findAllByDept(4);
-        Assertions.asserThat(employees.size()).isEqualTo(1);
-        Assertions.asserThat(employees.get()).isEqualTo(new Employee("test4", "test_test4", 50, 4));
+        Assertions.assertThat(employees.size()).isEqualTo(1);
+        Assertions.assertThat(employees.get(0)).isEqualTo(new Employee("test4", "test_test4", 50, 4));
+    }
+    @Test
+    testGroupByDept(){
+    Map<Integer, List<Employee>> actual = departmentService.groupByDept();
+        Assertions.assertThat(actual.keySet()).containsExactly(1, 2, 3, 4);
+        Assertions.assertThat(actual.get(1)).containsExactly(
+                new Employee("test", "test_test", 10, 1),
+                new Employee("test2", "test_test2", 30, 1),
+                new Employee("test5", "test_test5", 60, 1));
+
+        Assertions.assertThat(actual.get(2)).containsExactly(
+                new Employee("test1", "test_test1", 20, 2));
+
+        Assertions.assertThat(actual.get(3)).containsExactly(
+                new Employee("test3", "test_test3", 40, 3),
+                new Employee("test33", "test_test33", 5, 3));
+
+        Assertions.assertThat(actual.get(4)).containsExactly(
+                new Employee("test4", "test_test4", 50, 4));
     }
 
 }
+
